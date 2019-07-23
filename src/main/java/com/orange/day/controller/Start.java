c48480ff25acc7be48058edafe1d85e99691854d
SaltedFishSting <2050422401@qq.com>
2019-07-23 11:33:02 +0800

    start

--- src/main/java/com/orange/day/controller/Start.java
+++ src/main/java/com/orange/day/controller/Start.java
@@ -3,6 +3,7 @@ package com.orange.day.controller;
 public class Start {
     private String start="开始";
 
+    //https://blog.csdn.net/wwd0501/article/details/89497228
     private String end="结束";
 
 }
